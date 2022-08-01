91ef669004929111e95d18b71953b89c6fa4dfb5
SATISH DILIP SURNAR <SATISH DILIP SURNAR@LAPTOP-62LF9588>
2022-08-01 16:00:33 +0530

    second commit

---
+++ GetHubDay1/src/com/capgemini/EclipseWithGethub.java
@@ -0,0 +1,11 @@
+package com.capgemini;
+
+public class EclipseWithGethub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to capgemimi C2C program");
+
+	}
+
+}
