b637b9a5ba6299c57e08d434f1ac1ae9aeca555a
SATISH DILIP SURNAR <SATISH DILIP SURNAR@LAPTOP-62LF9588>
2022-09-20 09:13:29 +0530

    first commit

---
+++ SpringProject/src/com/geekyscript/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.geekyscript;
+
+public class HelloWorld {
+	public void method() {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ SpringProject/src/com/geekyscript/MainApp.java
@@ -0,0 +1,17 @@
+package com.geekyscript;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloworld");
+		obj.method();
+		
+
+	}
+
+}
